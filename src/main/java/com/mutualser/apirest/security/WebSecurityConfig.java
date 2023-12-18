3833c4bc9de9dd35c063d9f0b008401d4d7e6715
weslydavid <wescamilla@mutualser.org>
2023-12-18 09:14:28 -0500

    no message

---
+++ src/main/java/com/mutualser/apirest/security/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+/*package com.mutualser.apirest.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.Se
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterC
+}*/
